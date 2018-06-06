dd552e7ca62db53a067d862e7550f8e545ed2883
John Dominguez <john.dominguez@stonybrook.edu>
2018-06-06 16:45:42 -0400

    charts skeleton html

--- projects/revaturecloud/forecast/src/lib/charts/charts.component.ts
+++ projects/revaturecloud/forecast/src/lib/charts/charts.component.ts
@@ -7,7 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartsComponent implements OnInit {
 
-  constructor() { }
+  single = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    }
+  ];
+  // multi: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
+
+  constructor() {
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
 
   ngOnInit() {
   }
