2113f18b56c10c53f4ae056a88dd5bb0fcb9fb2a
John Dominguez <john.dominguez@stonybrook.edu>
2018-06-07 11:40:44 -0400

    [#158037408] Removed app.module dependencies, installed dependencies into RevatureCloud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ForecastModule } from 'projects/revaturecloud/forecast/src/public_api';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,14 +10,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-
-    NgbModule.forRoot(),
-
-    MatTabsModule,
-    MatCardModule,
-    MatIconModule,
-    BrowserAnimationsModule,
-
     ForecastModule
   ],
   providers: [],
