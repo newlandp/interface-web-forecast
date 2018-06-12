a963539e4a2d495dd51ca572ae9010791a010e9c
newlandp <paulnewland@hotmail.com>
2018-06-11 20:34:48 -0400

    created a separate component for pie chart [#158037408]

--- projects/revaturecloud/forecast/src/lib/forecast.module.ts
+++ projects/revaturecloud/forecast/src/lib/forecast.module.ts
@@ -16,6 +16,7 @@ import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import { RadarChartComponent } from './radar-chart/radar-chart.component';
     BarChartComponent,
     LineChartComponent,
     DoughnutChartComponent,
-    RadarChartComponent
+    RadarChartComponent,
+    PieChartComponent
   ],
   providers: [
     ForecastService
